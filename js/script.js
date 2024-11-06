665fa02052b01fb362699b9536caa1f48aba9c44
ktagdiwala <krishna.tagdiwala@gmail.com>
2024-11-05 20:30:27 -0800

    Updated styles

--- js/script.js
+++ js/script.js
@@ -4,12 +4,16 @@ document.querySelector("button").addEventListener("click", gradeQuiz);
 // Global Variables
 let score = 0;
 let attempts = localStorage.getItem("total_attempts");
+let unanswered = [];
 
 displayQ4Choices();
+displayQ8Choices();
 
 // Functions
 function displayQ4Choices(){
     let q4ChoicesArray = ["Maine", "Rhode Island", "Maryland", "Delaware"];
+
+    // Randomizes the option order
     q4ChoicesArray= _.shuffle(q4ChoicesArray);
     for (let i = 0; i < q4ChoicesArray.length; i++){
         document.querySelector("#q4Choices").innerHTML += `<input type="radio" name="q4" id= "${q4ChoicesArray[i]}" 
@@ -17,16 +21,29 @@ function displayQ4Choices(){
     }
 }//displayQ4Choices
 
-function isFormValid(){
+function displayQ8Choices(){
+    let q8ChoicesArray = ["Kentucky", "Tennessee", "Massachusetts", "Pennsylvania"];
+
+    // Randomizes the option order
+    q8ChoicesArray= _.shuffle(q8ChoicesArray);
+    for (let i = 0; i < q8ChoicesArray.length; i++){
+        document.querySelector("#q8Choices").innerHTML += `<input type="radio" name="q8" id= "${q8ChoicesArray[i]}" 
+        value="${q8ChoicesArray[i]}"> <label for="${q8ChoicesArray[i]}"> ${q8ChoicesArray[i]} </label>`
+    }
+}//displayQ8Choices
+
+function isFormValid(responses){
     let isValid = true;
-    if(document.querySelector("#q1").value==""){
-        isValid = false;
-        document.querySelector("#validationFdbk").innerHTML = "Question 1 was not answered";
+    for (let i=0; i<11; i++){
+        if(responses[i]==""){
+            isValid = false;
+            unanswered.push(i+1);
+        }
     }
-    if(document.querySelector("#q7").value==""){
-        isValid = false;
-        document.querySelector("#validationFdbk").innerHTML = "Question 7 was not answered";
+    if(unanswered.length!=0){
+        document.querySelector("#validationFdbk").textContent = "Questions unanswered: " + unanswered;
     }
+
     return isValid;
 }//isFormValid
 
@@ -34,21 +51,57 @@ function gradeQuiz(){
     document.querySelector("#feedback").textContent = "";
     console.log("Grading quiz…");
     document.querySelector("#validationFdbk").innerHTML = ""; // resets validation feedback
-    if(!isFormValid()){
-        return;
-    }
+    unanswered = []; // resets unanswered questions list
+
 
     // variables
     score = 0;
+    let responses = [];
+
     let q1Response = document.querySelector("#q1").value.toLowerCase();
     let q2Response = document.querySelector("#q2").value;
+    let q3Response = "temp"
+    // determine if q3 is unanswered
+        if(!document.querySelector("#Jefferson").checked && !document.querySelector("#Roosevelt").checked &&
+        !document.querySelector("#Jackson").checked && !document.querySelector("#Franklin").checked){
+            q3Response = "";
+        }
     let q4Response = document.querySelector("input[name=q4]:checked") ? document.querySelector("input[name=q4]:checked").value : "";
     let q5Response = document.querySelector("#q5").value;
+    let q6Response = "temp"
+    // determine if q6 is unanswered
+        if(!document.querySelector("#Pacific").checked && !document.querySelector("#Atlantic").checked &&
+            !document.querySelector("#Arctic").checked && !document.querySelector("#Indian").checked){
+            q6Response = "";
+        }
     let q7Response = document.querySelector("#q7").value.toLowerCase();
     let q8Response = document.querySelector("input[name=q8]:checked") ? document.querySelector("input[name=q8]:checked").value : "";
     let q9Response = document.querySelector("#q9").value;
+    let q10Response = document.querySelector("#q10").value;
+    let q11Response = document.querySelector("#q11").textContent;
+    console.log("q10Response: " + q11Response);
+    // determine if q11 is unanswered
+        if(q11Response == "Select a zone on the map"){
+            q11Response = "";
+        }
 
-    // console.log(q4Response);
+    // adds all responses to an array
+    responses.push(q1Response);
+    responses.push(q2Response);
+    responses.push(q3Response);
+    responses.push(q4Response);
+    responses.push(q5Response);
+    responses.push(q6Response);
+    responses.push(q7Response);
+    responses.push(q8Response);
+    responses.push(q9Response);
+    responses.push(q10Response);
+    responses.push(q11Response);
+
+
+    if(!isFormValid(responses)){
+        return;
+    }
 
     // Grading question 1
     if(q1Response == "sacramento"){
@@ -115,6 +168,22 @@ function gradeQuiz(){
         wrongAnswer(9);
     }
 
+    // Grading question 10
+    if(q10Response == 48){
+        rightAnswer(10);
+    }else{
+        wrongAnswer(10);
+    }
+
+    // Grading question 11
+    if(q11Response == "Zone 2"){
+        rightAnswer(11);
+        score +=1;
+    }else{
+        wrongAnswer(11);
+    }
+
+
     let totalScore = document.querySelector("#totalScore");
 
     totalScore.textContent = `Total Score: ${score}`;
@@ -122,22 +191,28 @@ function gradeQuiz(){
     localStorage.setItem("total_attempts", attempts);
 
     if(score < 80){
-        totalScore.style.color = 'red';
+        totalScore.style.color = '#d62828';
     }else{
-        totalScore.style.color = "green";
+        totalScore.style.color = "#b1df78";
         document.querySelector("#feedback").textContent = "Congratulations! You scored at least 80%";
     }
 }// gradeQuiz
 
 function rightAnswer(index){
     document.querySelector(`#q${index}Feedback`).innerHTML = "Correct";
-    document.querySelector(`#q${index}Feedback`).className = "bg-success text-white";
-    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/checkmark.png' alt='Checkmark'>";
-    score += 10;
+    document.querySelector(`#q${index}Feedback`).className = "correct d-inline feedback";
+    document.querySelector(`#markImg${index}`).innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"40\" width=\"40\" viewBox=\"0 0 512 512\"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill=\"#74c365\" d=\"M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM369 209L241 337c-9.4 9.4-24.6 9.4-33.9 0l-64-64c-9.4-9.4-9.4-24.6 0-33.9s24.6-9.4 33.9 0l47 47L335 175c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9z\"/></svg>";
+    score += 9;
 }
 
 function wrongAnswer(index){
     document.querySelector(`#q${index}Feedback`).innerHTML = "Incorrect!";
-    document.querySelector(`#q${index}Feedback`).className = "bg-warning text-white";
-    document.querySelector(`#markImg${index}`).innerHTML = "<img src='img/xmark.png' alt='xmark'>";
+    document.querySelector(`#q${index}Feedback`).className = "incorrect d-inline feedback";
+    document.querySelector(`#markImg${index}`).innerHTML = "<svg xmlns=\"http://www.w3.org/2000/svg\" height=\"40\" width=\"40\" viewBox=\"0 0 512 512\"><!--!Font Awesome Free 6.6.0 by @fontawesome - https://fontawesome.com License - https://fontawesome.com/license/free Copyright 2024 Fonticons, Inc.--><path fill=\"#d62828\" d=\"M256 512A256 256 0 1 0 256 0a256 256 0 1 0 0 512zM175 175c9.4-9.4 24.6-9.4 33.9 0l47 47 47-47c9.4-9.4 24.6-9.4 33.9 0s9.4 24.6 0 33.9l-47 47 47 47c9.4 9.4 9.4 24.6 0 33.9s-24.6 9.4-33.9 0l-47-47-47 47c-9.4 9.4-24.6 9.4-33.9 0s-9.4-24.6 0-33.9l47-47-47-47c-9.4-9.4-9.4-24.6 0-33.9z\"/></svg>";
+}
+
+function selectRegion(zone){
+    selection = document.querySelector('#q11');
+    selection.textContent = zone;
+    selection.style.color = "#36630E";
 }
